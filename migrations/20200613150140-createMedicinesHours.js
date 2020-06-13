8898d0a73ea41c78eeb35a95434a356bc7c51758
Paulo Henrique <henrique@formahomolog.com.br>
2020-06-13 12:10:59 -0300

    migrations

---
+++ migrations/20200613150140-createMedicinesHours.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('medicinesHours', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			medicinesId: {
+				allowNull: false,
+				type: Sequelize.INTEGER,
+			},
+			userId: {
+				allowNull: false,
+				type: Sequelize.INTEGER,
+			},
+			hours: {
+				allowNull: false,
+				type: Sequelize.DATETIME,
+			}
+		})
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('medicinesHours');
+	}
+};

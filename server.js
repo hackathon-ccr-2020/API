f5b7f3d866a614edbb8ad0dcf81eba1c440d9662
Fellipe Medeiros <fellipe.medeiros@hotmail.com>
2020-06-13 13:28:19 -0300

    wip sequelize

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const Friend = require('./models');
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+  console.log(Friend)
   res.json({ message: "Iae man." });
 });
-
-require("./app/routes/customer.routes.js")(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

3ccef906ea8f80042677c0764cf188f9e88b1a33
Paulo Henrique <henrique@formahomolog.com.br>
2020-06-13 09:38:56 -0300

    first commit

---
+++ app/routes/customer.routes.js
@@ -0,0 +1,7 @@
+module.exports = app => {
+  const customers = require("../controllers/customer.controller.js");
+
+  app.post("/customers", customers.create);
+
+  app.get("/customers", customers.findAll);
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Iae man." });
+});
+
+require("./app/routes/customer.routes.js")(app);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});

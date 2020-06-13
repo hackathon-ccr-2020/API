e946ebfc38208987a94f6f4a6178042a5a7080e9
Vinicius Luiz <viniciuslcp97@gmail.com>
2020-06-13 13:40:43 -0300

    add channels and channel content routes

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Iae man." });
 });
 
+require("./routes/channel.routes.js")(app);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);

7faeae933d66328a1a826f9242c7aaf7a74e1f42
Vinicius Luiz <viniciuslcp97@gmail.com>
2020-06-13 18:19:26 -0300

    add create user endpoint

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+
 module.exports = (sequelize, DataTypes) => {
    const User = sequelize.define('User', {
         id: {
@@ -29,7 +31,20 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false,
             type: DataTypes.STRING
         },
-   }, {timestamps: false, tableName: "users"});
+   }, {
+        timestamps: false,
+        tableName: "users"
+    });
+
+    User.beforeCreate((user, options) => {
+        return bcrypt.hash(user.password, 10)
+            .then(hash => {
+                user.password = hash;
+            })
+            .catch(err => {
+               throw new Error();
+            });
+   })
 
     User.associate = function (models) {
         User.belongsToMany(models.Channel, { as: 'Channels', through: { model: models.ChannelUser }, foreignKey: 'user_id'})
